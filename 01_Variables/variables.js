f7d28f9d3ba0ebcd9f1af7ee2d0806602d311bbc
atsuchak <suchak0630@gmail.com>
2026-02-11 18:08:02 +0600

    javascript variables, datatype added

---
+++ 01_Variables/variables.js
@@ -0,0 +1,39 @@
+//number datatype
+let age = 18;
+let gpa = 3.58;
+
+console.log(`Number Datatype:`);
+console.log(`Age: ${age}`);
+console.log(`Type of age: ` + typeof age);
+console.log(`GPA: ${gpa}`);
+console.log(`Type of gpa: ` + typeof gpa + `\n`);
+
+
+//string datatype
+let firstName = "Human";
+let email = "human@gmail.com"
+
+console.log(`String Datatype:`);
+console.log(`Name: ${firstName}`);
+console.log(`Type of name: ` + typeof firstName);
+console.log(`Email: ${email}`);
+console.log(`Type of email: ` + typeof email + `\n`);
+
+
+//boolean datatype
+let pass = true;
+
+console.log(`He pass the exam: ${pass}`);
+console.log(`Type of pass: ` + typeof pass);
+
+
+//Overview
+let studentName = "Ahnaf Tajwar Suchak";
+let studentEmail = "ahnaftajwarsuchak@gmail.com";
+let studentId = 87;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `Student name: ${studentName}`;
+document.getElementById("p2").textContent = `Student email: ${studentEmail}`;
+document.getElementById("p3").textContent = `Student id: ${studentId}`;
+document.getElementById("p4").textContent = `Student status: ${isStudent}`;
