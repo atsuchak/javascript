efce57530d8172abc1893021bff0f23f50f9a078
atsuchak <suchak0630@gmail.com>
2026-02-15 23:01:52 +0600

    build a counter program

---
+++ 06_Counter_Program/script.js
@@ -0,0 +1,24 @@
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const resetBtn = document.getElementById("reset");
+const countLabel = document.getElementById("counter");
+
+let count = 0;
+
+increaseBtn.onclick = function() {
+    count++;
+
+    countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function() {
+    count--;
+
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function() {
+    count = 0;
+    
+    countLabel.textContent = count;
+}
