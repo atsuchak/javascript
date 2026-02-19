692b824ecf99911b6c7f372a634339b5547b3842
atsuchak <suchak0630@gmail.com>
2026-02-20 00:17:54 +0600

    add initial JavaScript file for Conditional Statement concepts

---
+++ 09_Conditional_Statement/script.js
@@ -0,0 +1,9 @@
+let age = 18;
+if(age >= 18) {
+    if(age == 18) console.log(`User is able to vote for the first time`);
+    else console.log(`User is able to vote`);
+}else console.log(`User is unable to vote`);
+
+let isStudent = false;
+if(isStudent) console.log(`User is a student`);
+else console.log(`User is not a student`);
