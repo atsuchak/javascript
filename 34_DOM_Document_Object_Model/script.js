ed67754b47b47520cdfbddc3636e7d4a8f93498e
atsuchak <suchak0630@gmail.com>
2026-04-09 00:25:29 +0600

    feat: add HTML, CSS, and JavaScript files for DOM manipulation demonstration

---
+++ 34_DOM_Document_Object_Model/script.js
@@ -0,0 +1,10 @@
+// console.dir(document.body);
+// console.log(`Everything works perfectly`);
+
+let paragraphs = document.querySelector(".paragraph");  //using class
+console.log(paragraphs);
+console.dir(paragraphs);
+
+let allParagraphs = document.querySelectorAll(".paragraph");  //using class(use . for get class elements)
+console.log(allParagraphs);
+console.dir(allParagraphs);
